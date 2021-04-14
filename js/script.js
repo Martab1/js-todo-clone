626cd4022760314d71bfe16b993cdb7221c5b4e8
Martab1 <martabernardo86@gmail.com>
2021-04-14 14:45:53 +0200

    create repo & init base layout

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+
+/* 
+
+TO DO LIST 
+
+popolando gli elementi della lista con JavaScript
+inserendo un nuovo todo con un input testuale e gli eventi da tastiera
+Rimozione todo con click su icona
+Cliccando sul testo compare uno sbarramento a indicarne il completamento
+
+
+*/
+
